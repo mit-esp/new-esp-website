f72e8224a368da9aea246c77289bf410ef472d09
kyfantaz <kyfantaz@users.noreply.github.com>
2021-11-30 15:27:36 -0500

    removes unnecessary create react app assets; swaps out for relativedelta

--- esp/validators.py
+++ esp/validators.py
@@ -4,11 +4,11 @@ from datetime import timedelta
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-
+from dateutil.relativedelta import relativedelta
 
 def validate_graduation_year(value):
-    earliest_allowed = (timezone.now() - timedelta(days=round(50 * 365.25))).year  # 50 years
-    latest_allowed = (timezone.now() + timedelta(days=round(15 * 365.25))).year  # 15 years
+    earliest_allowed = (timezone.now() - relativedelta(years=round(100))).year  # 100 years
+    latest_allowed = (timezone.now() + relativedelta(years=round(15))).year  # 15 years
     if not earliest_allowed <= value <= latest_allowed:
         raise ValidationError(
             _('%(value)s is not a valid graduation year. Must be between %(earliest_allowed)s - %(latest_allowed)s'),
